10f410d72dcc5ece5c3c8d1a63dfa564094085d2
Arthur Vargas <arthurvargas@Mac.attlocal.net>
2025-08-02 20:37:39 -0500

    new routes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 from flask import Flask
+from datetime import date
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
@@ -22,7 +24,26 @@ def home():
 
 '''
 
+@app.route('/exam/passers/list/<float:rate>/<uuid:doc>')
+def report_exam_passers(rating: float, doc_id:uuid4=None):
+    exams = list_passing_scores(rating)
+    response = make_response(
+        render_template('exam/list_exam_passers.html', exams=exams, doc_id=doc_id), 200
+    )
+    return response
 
+@app.route('/certificate/accomp/<string:name>/<string:course>/<date:accomplished_date>')
+def show_certificate(name:str, course:str, accomplished_date:date):
+    certificate = """
+        <html><head><title>Certificate of Accomplishment</title></head>
+            <body>
+                <h1>Certificate of Accomplishment</h1>
+                <p>The participant {} is, hereby awarded this certificate of accomplishment,
+                in {} course on {} date for passing all exams. He/she proved to be ready for
+                any of his/her future endeavors.</em>
+            </body>
+        </html>
+    """.format(name, course, accomplished_date)
 
 
 if __name__ == "__main__":
